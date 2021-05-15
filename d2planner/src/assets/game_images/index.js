ee9db433d3f69c08c865d11c1b83dde496874707
Dustin Winslow <dmwinslow@gmail.com>
2021-05-15 17:28:06 +0100

    Implement images index

---
+++ d2planner/src/assets/game_images/index.js
@@ -0,0 +1,9 @@
+import amazonTree1 from './amazon_tree_1.jpg';
+import amazonTree2 from './amazon_tree_2.jpg';
+import amazonTree3 from './amazon_tree_3.jpg';
+
+export default {
+  amazonTree1,
+  amazonTree2,
+  amazonTree3,
+};

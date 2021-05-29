ebd4c6bb1cada9e3446a780a69fef21b70b8de67
Dustin Winslow <dmwinslow@gmail.com>
2021-05-29 16:01:52 +0100

    Implement initial formatters

---
+++ d2planner/src/formatDesclines.js
@@ -0,0 +1,34 @@
+import formattersByDescline from './formatters';
+
+function formatDesclines (root, skill, lvl, skillLevels) {
+  if (skill[`${root}Lines`] === undefined) {
+    return null;
+  }
+  let lines = [];
+  for (const entry of skill[`${root}Lines`]) {
+    const line = formatDescline(
+      entry[`${root}Line`],
+      skill,
+      lvl,
+      skillLevels,
+      entry[`${root}TextA`],
+      entry[`${root}TextB`],
+      entry[`${root}CalcA`],
+      entry[`${root}CalcB`],
+    );
+    if (line !== null) {
+      lines.push(line);
+    }
+  }
+  if (lines.length === 0) {
+    return null;
+  }
+  return lines.filter(line => line !== null).join('\n');
+}
+
+function formatDescline (desclineNumber, skill, lvl, skillLevels, ta, tb, ca, cb) {
+  const formatter = formattersByDescline[desclineNumber];
+  return formatter(skill, lvl, skillLevels, ta, tb, ca, cb);
+}
+
+export default formatDesclines;
---
+++ d2planner/src/formatters.js
@@ -0,0 +1,53 @@
+import calculateSkillValue from './calculateSkillValue'
+
+const framesPerSecond = 25;
+const yardsPerGameUnit = 2 / 3;
+
+const formattersByDescline = {
+  6: createCalcFormatter((ta, tb, ca, cb) => (`+${ca} ${ta}`)),
+};
+
+
+function createCalcFormatter (template, frames=false, gameUnits=false, precision=null, multiplier=null) {
+  function formatter(skill, lvl, skillLevels, ta, tb, ca, cb) {
+    let calcA = calculateSkillValue(ca, skill, lvl, skillLevels);
+    let calcB = calculateSkillValue(cb, skill, lvl, skillLevels);
+
+    calcA = convertCalc(calcA, frames, gameUnits, precision, multiplier);
+    calcB = convertCalc(calcB, frames, gameUnits, precision, multiplier);
+
+    return template(ta, tb, calcA, calcB);
+  }
+  return formatter;
+}
+
+
+function convertCalc(calc, frames, gameUnits, precision, multiplier) {
+    if (calc === undefined) {
+      return calc;
+    }
+    if (frames) {
+      calc /= framesPerSecond;
+    }
+    if (gameUnits) {
+      calc = floor(calc) * yardsPerGameUnit;
+    }
+    if (multiplier !== null) {
+      calc *= multiplier;
+    }
+    if (precision !== null) {
+      calc = floor(calc, precision);
+    }
+    return calc;
+}
+
+
+function floor(x, precision=null) {
+  if (precision === null) {
+    return Math.floor(x);
+  }
+  return Math.floor(precision * x) / precision;
+}
+
+
+export default formattersByDescline;

fcb4acf8cd9abb5fb079395648dd5e0fce558864
Dustin Winslow <dmwinslow@gmail.com>
2021-06-01 08:32:31 +0100

    Implement formatters 1-15

--- d2planner/src/formatters.js
+++ d2planner/src/formatters.js
@@ -1,5 +1,10 @@
 import calculateSkillValue from './calculateSkillValue'
-import { calculateToHit } from './calculators'
+import {
+  calculateLength,
+  calculateManaCost,
+  calculateToHit,
+  framesPerSecond,
+} from './calculators'
 import {
   calculatePhysicalDamageMin,
   calculatePhysicalDamageMax,
@@ -7,33 +12,45 @@ import {
   calculateElementalDamageMax,
 } from './damageCalculators';
 
-const framesPerSecond = 25;
 const yardsPerGameUnit = 2 / 3;
 
+// Commented values are not used in 1.14D
+// these should be uncommented and tested if used in other patches
 const formattersByDescline = {
+  1: formatManaCost,
   2: createCalcFormatter({template: (ta, tb, calcA, calcB) => (`${ta} ${calcA}${tb}`), precision: 1}),
-
+  3: createCalcFormatter({template: (ta, tb, calcA, calcB) => (`${ta}${calcA}${tb}`), precision: 1}),
+  4: createCalcFormatter({template: (ta, tb, calcA, calcB) => (`${ta}+${calcA}`)}),
   5: createCalcFormatter({template: (ta, tb, calcA, calcB) => (`${ta} ${calcA}`), precison: 1}),
   6: createCalcFormatter({template: (ta, tb, calcA, calcB) => (`+${calcA} ${ta}`)}),
-
+  7: createCalcFormatter({template: (ta, tb, calcA, calcB) => (`${calcA} ${ta}`)}),
   8: formatAttackRating,
   9: formatPhysicalDamage,
   10: formatElementalDamage,
-
+  11: formatElementalLength,
+  12: createCalcFormatter({template: (ta, tb, calcA, calcB) => (`${ta}${calcA} seconds`), frames: true, precison: 10}),
+  13: formatMinionLife,
+  14: formatPoisonDamage,
+  // 15: (skill, lvl, skillLevels, ta, tb, ca, cb) => (`${ta}:${tb}`),
   66: createFillTaWithCalcAFormatter('%d%'),
 };
 
-function formatAttackRating(skill, lvl, skillLevels, ta, tb, ca, cb) {
+function formatManaCost (skill, lvl, skillLevels, ta, tb, ca, cb) {
+  const cost = floor(calculateManaCost(skill, lvl, skillLevels), ca || 10);
+  return cost ? `${skill.strMana}${cost}` : null;
+}
+
+function formatAttackRating (skill, lvl, skillLevels, ta, tb, ca, cb) {
   const attackRating = calculateToHit(skill, lvl, skillLevels);
   if (attackRating === undefined) {
-    return '';
+    return null;
   }
   return `Attack +${attackRating} percent`;
 }
 
 function formatPhysicalDamage (skill, lvl, skillLevels, ta, tb, ca, cb) {
   if (skill.minDam === undefined) {
-    return '';
+    return null;
   }
   const minDamage = floor(calculatePhysicalDamageMin(skill, lvl, skillLevels));
   const maxDamage = floor(calculatePhysicalDamageMax(skill, lvl, skillLevels));
@@ -46,7 +63,7 @@ function formatPhysicalDamage (skill, lvl, skillLevels, ta, tb, ca, cb) {
 
 function formatElementalDamage (skill, lvl, skillLevels, ta, tb, ca, cb) {
   if (skill.eMin === undefined) {
-    return '';
+    return null;
   }
   const minDamage = floor(calculateElementalDamageMin(skill, lvl, skillLevels));
   const maxDamage = floor(calculateElementalDamageMax(skill, lvl, skillLevels));
@@ -55,6 +72,33 @@ function formatElementalDamage (skill, lvl, skillLevels, ta, tb, ca, cb) {
     return `${skill.eType} Damage: +${minDamage}`
   }
   return `${skill.eType} Damage: ${minDamage}-${maxDamage}`
+}
+
+function formatElementalLength (skill, lvl, skillLevels, ta, tb, ca, cb) {
+  if (skill.eLen === undefined) {
+    return null;
+  }
+  const length = calculateLength(skill, lvl, skillLevels) / framesPerSecond;
+  return `${skill.eType} Length: ${length} seconds`;
+}
+
+function formatPoisonDamage (skill, lvl, skillLevels, ta, tb, ca, cb) {
+  if (skill.eMin === undefined) {
+    return null;
+  }
+  const length = calculateLength(skill, lvl, skillLevels);
+  const minDamage = floor(length * calculateElementalDamageMin(skill, lvl, skillLevels));
+  const maxDamage = floor(length * calculateElementalDamageMax(skill, lvl, skillLevels));
+  const formattedLength = length / framesPerSecond;
+  return `Poison Damage : ${minDamage}-${maxDamage}\nover ${formattedLength} seconds`;
+}
+
+function formatMinionLife (skill, lvl, skillLevels, ta, tb, ca, cb) {
+  const baseLife = (skill.summon.minHPNormal + skill.summon.maxHPNormal) / 2;
+  const calcA = calculateSkillValue(ca, skill, lvl, skillLevels) || 0;
+  const calcB = calculateSkillValue(cb, skill, lvl, skillLevels) || 0;
+  const life = floor((1 + calcA / 100) * (baseLife + calcB));
+  return `Life: ${life}`;
 }
 
 function createFillTaWithCalcAFormatter (pattern) {
@@ -72,8 +116,7 @@ function createCalcFormatter ({template, frames=false, gameUnits=false, precisio
 
     calcA = convertCalc(calcA, frames, gameUnits, precision, multiplier);
     calcB = convertCalc(calcB, frames, gameUnits, precision, multiplier);
-
-    return template(ta, tb, calcA, calcB);
+    return (calcA || calcB) ? template(ta, tb, calcA, calcB) : null;
   }
   return formatter;
 }

d98edda1ec820ac557337631778f7f53265087c1
Dustin Winslow <dmwinslow@gmail.com>
2021-06-13 11:53:37 +0100

    Revert "Merge pull request #28 from d2planner/add-github-pages"
    This reverts commit f314acbbf75b811c0644eeefed46f18b2a6df001, reversing
    changes made to f7443415097a565df3b108a6892a69e036a31ad5.

--- d2planner/src/ShareButton.js
+++ d2planner/src/ShareButton.js
@@ -19,7 +19,7 @@ function ShareButton (props) {
         effect='solid'
         event='click'
         eventOff='mouseleave'
-        afterShow={() => navigator.clipboard.writeText(`d2planner.github.io/skills/${props.buildString}`)}
+        afterShow={() => navigator.clipboard.writeText(`d2planner.github.io/${props.buildString}`)}
         delayHide={500}
         type='light'
         textColor='#404040'
